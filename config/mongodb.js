7d6bbb4c42b3725a4c67b2647b0a99e8b759e056
BhavyaSreeJallolla <bhavyasreejallolla967@gmail.com>
2024-10-15 19:55:45 +0530

    Describe your changes here

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,10 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
